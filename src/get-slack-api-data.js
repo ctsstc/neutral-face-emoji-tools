bdc9408860d0583eb75f4cbb0b94ba6c0e34631b
Cody Swartz <ctsstc@gmail.com>
2019-03-22 02:55:16 -0700

    Directly access slack token and version uid

--- src/get-slack-api-data.js
+++ src/get-slack-api-data.js
@@ -1,27 +1,6 @@
+
 export default function getSlackApiData () {
-  /** @type {NodeListOf<HTMLScriptElement>} */
-  const scripts = document.querySelectorAll('script[type="text/javascript"]');
-  let apiToken;
-  let versionUid;
-
-  scripts.forEach((script) => {
-    const isBootDataScript = /var\sboot_data\s\=\s\{/.test(script.innerText);
-
-    if (!isBootDataScript) {
-      return;
-    }
-
-    const apiTokenResult = /["]?api_token["]?\:\s*\"(.+?)\"/g.exec(script.innerText);
-    const versionUidResult = /["]?version_uid["]?\:\s*\"(.+?)\"/g.exec(script.innerText);
-    
-    if (apiTokenResult) {
-      apiToken = apiTokenResult[1];
-    }
-
-    if (versionUidResult) {
-      versionUid = versionUidResult[1];
-    }
-  });
+  const { api_token: apiToken, version_uid: versionUid } = boot_data;
 
   return {
     apiToken,
